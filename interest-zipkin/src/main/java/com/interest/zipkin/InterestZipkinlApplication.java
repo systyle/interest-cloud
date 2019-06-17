e749d8a5e22ac07a05c2d0ce7fb67a1792bda9cc
Scott Sun (孙浩) <sunhao.lc@inspur.com>
2019-06-17 19:03:44 +0800

    Initial commit

---
+++ interest-zipkin/src/main/java/com/interest/zipkin/InterestZipkinlApplication.java
@@ -0,0 +1,16 @@
+package com.interest.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import zipkin.server.internal.EnableZipkinServer;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableZipkinServer
+public class InterestZipkinlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterestZipkinlApplication.class, args);
+    }
+
+}
